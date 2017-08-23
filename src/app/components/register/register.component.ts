c5deaee145d64eaa8c4c195533f728fa520a0f32
Mark <mark@Marks-MacBook-Pro.local>
2017-08-23 13:28:35 -0700

    added forms registration and report, and functional select menus

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { JobService } from '../../services/job';
+import { Job } from '../../models/job';
 import { ColonistService } from '../../services/colonist';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,20 +15,44 @@ import { ColonistService } from '../../services/colonist';
 })
 export class RegisterComponent implements OnInit {
 
+  jobs: Job[] = [];
+
+  registerForm = new FormGroup({
+    name: new FormControl('Name'),
+    age: new FormControl('Age'),
+    job_id: new FormControl('Select Occupation'),
+  });
+
   constructor(private jobService: JobService, 
     private colonistService: ColonistService
   ) { }
 
   async ngOnInit() {
-    const data = {
-      name: 'data test',
-      age: '1234',
-      job_id: '1'
-    };
 
-    const Job = await this.jobService.getJobs();
-    const newColonist = await this.colonistService.registerColonist(data);
-    console.log(Job);
-    console.log(newColonist);
+    this.jobs = await this.jobService.getJobs();
   }
 }
+
+
+
+
+
+
+
+// removeListItem(item){
+//   this.data = this.data.filter(li => li !== item);
+// }
+// this.data is the list and that is removing an item from the list
+
+
+// setInterval(()=>{
+//       this.data.push({text: `encounter ${Math.random()}`})
+//     }, 1000);
+
+
+// <input #text/><button (click)='addItem(text.value)'></button>
+// addItem(item){
+//   this.data.push({ text: item })
+// }
+
+// this will grab the input value and add it on click from button
--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AlienService } from '../../services/alien';
 import { ReportService } from '../../services/encounters';
+import { Alien } from '../../models/alien';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-report',
@@ -13,19 +15,19 @@ import { ReportService } from '../../services/encounters';
 })
 export class ReportComponent implements OnInit {
 
+  aliens: Alien[] = [];
+
+   registerForm = new FormGroup({
+    alien: new FormControl('Select alien type'),
+    post: new FormControl('Select Occupation'),
+  });
+
   constructor(private alienService: AlienService, private reportService: ReportService) { }
 
-  async ngOnInit() {                //ES6 same as promise
-    const data = {
-      date: '2020-10-10',
-      colonist_id: '3',
-      atype: 'spider',
-      action: 'ham'
-    }
 
-    const newReport = await this.reportService.newReport(data);
-    const aliens = await this.alienService.getAliens();
-    console.log(aliens);
-    console.log(newReport);
+
+  async ngOnInit() {                //ES6 same as promise
+    
+    this.aliens = await this.alienService.getAliens();
   }
 }
